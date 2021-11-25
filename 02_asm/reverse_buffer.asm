d4f920fdfc4fab1774bebd42c89473ad700c007b
Kristóf Bácskai <45177246+Kris030@users.noreply.github.com>
2021-11-25 20:28:12 +0100

    Add comments to reverse_buffer

--- 02_asm/reverse_buffer.asm
+++ 02_asm/reverse_buffer.asm
@@ -1,31 +1,41 @@
 ; buff pointer in ebx, buff end in ecx
 reverse_buffer:
 
+; variable tmp1
 	push eax
+; variable p1
 	push ebx
+; variable p2
 	push ecx
-	
-; tmp
+; variable tmp2
 	push edx
 
+; --- LOOP START ---
+; while (p1 < p2)
 	.loop:
 	cmp ebx, ecx
 	jge .loop_end
 
+; tmp2 = *p2
 	mov dl, byte [ecx]
 
+; *p1 = *p2
 	mov al, byte [ebx]
 	mov byte [ecx], al
 
+; p2--
 	dec ecx
 
+; *p1 = tmp2
 	mov byte [ebx], dl
 
+; p1++
 	inc ebx
-
 	jmp .loop
+; --- LOOP END ---
 
 	.loop_end:
+; clean up variables
 	pop edx
 	pop ecx
 	pop ebx
