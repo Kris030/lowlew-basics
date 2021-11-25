fe0ce92e0032f7160fab44c9aeaf3aff46f878b8
Kristóf Bácskai <45177246+Kris030@users.noreply.github.com>
2021-11-25 20:40:25 +0100

    Add comments to num_to_string
    Comments done (for now).
    still TODO: 16 bit gdb

--- 02_asm/num_to_string.asm
+++ 02_asm/num_to_string.asm
@@ -1,12 +1,14 @@
 
-; number in ebx, buffer in ecx
+; number in ebx, buffer in ecx, returns result's length
 num_to_string:
 
 ; count the length in esi
 	push esi
-	xor esi, esi
+	mov esi, 0
 
+; argument N
 	push ebx
+; save ecx, edx
 	push ecx
 	push edx
 
@@ -14,20 +16,28 @@ num_to_string:
 ; count++
 	inc esi
 
-	; DIVIDE
+; --- DIVIDE ---
+; divide eax, result in eax
 	mov eax, ebx
+; by 10
 	mov ebx, 10
-	xor edx, edx
-
+; remainder in edx
+	mov edx, 0
+; run division
 	div ebx
-	; DIVIDE END
+; --- DIVIDE END ---
 
+; add '0' to remainder to get current digit
 	add edx, '0'
+; move it to buffer
 	mov [ecx], edx
+; advance buffer
 	inc ecx
 
+; restore N into ebx
 	mov ebx, eax
 
+; while (N > 0)
 	cmp eax, 0
 	jg .loop
 
@@ -36,6 +46,7 @@ num_to_string:
 ; return length
 	mov eax, esi
 
+; restore registers
 	pop edx
 	pop ecx
 	pop ebx
@@ -44,10 +55,15 @@ num_to_string:
 	mov ebx, ecx
 	add ecx, esi
 	dec ecx
+; save return value
+	push eax
 	call reverse_buffer
 
+; restore esi and return value
+	pop eax
 	pop esi
 
 	ret
 
 %include "reverse_buffer.asm"
+
