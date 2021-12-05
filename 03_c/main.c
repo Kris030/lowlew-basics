9b8656350298624e78a5ce4cd1d741be5e78bb17
the boi <45177246+Kris030@users.noreply.github.com>
2021-12-05 14:33:45 +0100

    Finish it later!!!

--- 03_c/main.c
+++ 03_c/main.c
@@ -1,8 +1,6 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
 // ------ basic syntax ------
+#include <stdio.h>
+
 #define MY_CONST 654
 #define MY_MACRO(a, b) (a) + (b)
 
@@ -10,16 +8,31 @@ typedef struct {
 	int a, b, c;
 } MyStruct;
 
+void func() {
+	puts("hello world");
+}
+
 int main() {
 	
 	int a = 65;
 	char c = 'a';
 	char s[] = "hello world";
+	int i[10];
 
 	int *p = &a;
 	int v = *p;
+
+	func();
 	// ------ basic syntax ------
 
-	
+
+	// ------ pointer fun ------
+
+	// ------ pointer fun ------
+
+
+	// ------ dynamic memory ------
+
+	// ------ dynamic memory ------
 
 }
